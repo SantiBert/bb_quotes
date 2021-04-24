500bc96a8511aef5bf8847e8993fe56c2fcb9874
SantiBert <santibertero34@gmail.com>
2021-04-24 15:50:40 -0300

    tercer commit

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -1,15 +1,34 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+const ContenteQuote = styled.div`
+    padding:3rem;
+    border-radius: .5rem;
+    background-color: #fff;
+    max-width:800px;
+    margin-top:10rem;
+
+    @media (min-width: 992px){
+        margin-top:10rem;
+    }
+    h1{
+        font-family:Arial, Helvetica, sans-serif;
+        text-align: center;
+        position:relative;
+        padding-left: 4rem;
+    }
+`;
 
 const Quote = ({ quote }) => {
     return (
-        <div>
+        <ContenteQuote>
             <h1>
                 {quote.quote}
             </h1>
             <p>
                 {quote.author}
             </p>
-        </div>
+        </ContenteQuote>
     );
 }
 
